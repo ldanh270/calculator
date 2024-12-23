d7b8e25791c4dc83f6b6a4bc8a4696efa6eb7bc9
ldanh270 <ducanhle.dn@gmail.com>
2024-12-24 00:47:30 +0700

    done calculate function, calc btns, C, CE

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const storage = document.querySelector("#storage")
-const operator = document.querySelector("#operator")
-const current = document.querySelector("#current")
+const storageDisplay = document.querySelector("#storage")
+const operatorDisplay = document.querySelector("#operator")
+const currentDisplay = document.querySelector("#current")
 
 const clearAllBtn = document.querySelector("#C")
 const clearEntryBtn = document.querySelector("#CE")
 const percentBtn = document.querySelector("#percent")
 
-const calcBtns = document.querySelectorAll(".calc-btn")
+const operatorBtns = document.querySelectorAll(".calc-btn")
 
 const calculateBtn = document.querySelector("#equal")
 const addBtn = document.querySelector("#add")
@@ -18,28 +18,60 @@ const inputBtns = document.querySelectorAll(".num-btn")
 
 const deleteBtn = document.querySelector("#delete")
 
-const currentResult = () => {}
+let tempValue = ""
+let previousOperator = ""
 
-const addToStorage = (currentResult) => {}
+const calculateResult = (currentValue, storageValue, operator) => {
+  if (storageValue === "") return currentValue
+  if (currentValue === "") return currentValue
 
-const showCurrentOperator = (operator) => {}
-
-const clearCurrent = () => {}
-
-const main = () => {
-  inputBtns.forEach((inputBtn) => {
-    inputBtn.addEventListener("click", () => {
-      current.value += inputBtn.value
-      console.log(typeof current.value)
-    })
-  })
-  calcBtns.forEach((operator) => {
-    operator.addEventListener("click", () => {
-      addToStorage(currentResult)
-      showCurrentOperator(operator.innerText)
-      clearCurrent()
-    })
-  })
+  const current = Number(currentValue)
+  const storage = Number(storageValue)
+  console.log(storage, current, operator)
+  let result = NaN
+  switch (operator) {
+    case "+":
+      result = storage + current
+      break
+    case "-":
+      result = storage - current
+      break
+    case "*":
+      result = storage * current
+      break
+    case "÷":
+      result = storage / current
+      break
+  }
+  return result
 }
 
-main()
+calculateBtn.addEventListener("click", () => {
+  storageDisplay.value = calculateResult(currentDisplay.value)
+})
+
+inputBtns.forEach((inputBtn) => {
+  inputBtn.addEventListener("click", () => {
+    currentDisplay.value += inputBtn.value
+    tempValue += inputBtn.value
+  })
+})
+
+operatorBtns.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    storageDisplay.value = calculateResult(currentDisplay.value, storageDisplay.value, previousOperator)
+    currentDisplay.value = ""
+    operatorDisplay.value = operator.innerText
+    previousOperator = operator.innerText
+  })
+})
+
+clearAllBtn.addEventListener("click", () => {
+  storageDisplay.value = ""
+  currentDisplay.value = ""
+  operatorDisplay.value = ""
+})
+
+clearEntryBtn.addEventListener("click", () => {
+  currentDisplay.value = ""
+})
