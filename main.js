1c3f6289b9aafa7eef3c8b3b86b6c14669938130
ldanh270 <ducanhle.dn@gmail.com>
2024-12-25 15:06:05 +0700

    done all function

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const currentDisplay = document.querySelector("#current")
 
 const clearAllBtn = document.querySelector("#C")
 const clearEntryBtn = document.querySelector("#CE")
+const deleteBtn = document.querySelector("#delete")
+const dotBtn = document.querySelector("#dot")
 const percentBtn = document.querySelector("#percent")
 
 const operatorBtns = document.querySelectorAll(".calc-btn")
 
-const calculateBtn = document.querySelector("#equal")
+const equalBtn = document.querySelector("#equal")
 const addBtn = document.querySelector("#add")
 const minusBtn = document.querySelector("#minus")
 const multipleBtn = document.querySelector("#multiple")
@@ -16,19 +18,21 @@ const divideBtn = document.querySelector("#divide")
 
 const inputBtns = document.querySelectorAll(".num-btn")
 
-const deleteBtn = document.querySelector("#delete")
-
-let tempValue = ""
 let previousOperator = ""
+let newNumberStatus = false
+
+// calculate result processing
 
 const calculateResult = (currentValue, storageValue, operator) => {
-  if (storageValue === "") return currentValue
-  if (currentValue === "") return currentValue
+  let current = 0
+  if (currentValue.includes("%")) current = parseFloat(parseFloat(currentValue) / 100)
+  else current = parseFloat(currentValue)
+  const storage = parseFloat(storageValue)
 
-  const current = Number(currentValue)
-  const storage = Number(storageValue)
-  console.log(storage, current, operator)
-  let result = NaN
+  if (storageValue === "") return current
+  if (currentValue === "") return storage
+
+  let result = 0
   switch (operator) {
     case "+":
       result = storage + current
@@ -46,25 +50,62 @@ const calculateResult = (currentValue, storageValue, operator) => {
   return result
 }
 
-calculateBtn.addEventListener("click", () => {
-  storageDisplay.value = calculateResult(currentDisplay.value)
-})
+// input value processing
+
+const inputValue = (inputBtn) => {
+  if (newNumberStatus === false) {
+    currentDisplay.value += inputBtn.value
+  } else {
+    currentDisplay.value = inputBtn.value
+  }
+  newNumberStatus = false
+}
+
+// number buttons events
 
 inputBtns.forEach((inputBtn) => {
   inputBtn.addEventListener("click", () => {
-    currentDisplay.value += inputBtn.value
-    tempValue += inputBtn.value
+    inputValue(inputBtn)
   })
 })
+
+// percent button events
+
+percentBtn.addEventListener("click", () => {
+  const percentResult = currentDisplay.value / 100
+  currentDisplay.value += "%"
+})
+
+// dot button events
+
+dotBtn.addEventListener("click", () => {
+  if (currentDisplay.value.includes(".")) return
+  else inputValue(dotBtn)
+})
+
+// equal button events
+
+equalBtn.addEventListener("click", () => {
+  currentDisplay.value = calculateResult(currentDisplay.value, storageDisplay.value, previousOperator)
+  storageDisplay.value = ""
+
+  operatorDisplay.value = ""
+  previousOperator = operator.innerText
+  newNumberStatus = true
+})
+
+// operator buttons events
 
 operatorBtns.forEach((operator) => {
   operator.addEventListener("click", () => {
     storageDisplay.value = calculateResult(currentDisplay.value, storageDisplay.value, previousOperator)
-    currentDisplay.value = ""
     operatorDisplay.value = operator.innerText
     previousOperator = operator.innerText
+    newNumberStatus = true
   })
 })
+
+// CE button events
 
 clearAllBtn.addEventListener("click", () => {
   storageDisplay.value = ""
@@ -72,6 +113,14 @@ clearAllBtn.addEventListener("click", () => {
   operatorDisplay.value = ""
 })
 
+// C button events
+
 clearEntryBtn.addEventListener("click", () => {
   currentDisplay.value = ""
+})
+
+// delete button events
+
+deleteBtn.addEventListener("click", () => {
+  currentDisplay.value = currentDisplay.value.slice(0, -1)
 })
